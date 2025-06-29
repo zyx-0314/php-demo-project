2a41e398b15a3e6f1a78585858fad96cc33c45ec
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-06-29 18:59:01 +0800

    feat: Load database connection parameters from environment variables in postgreChecker.handler.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,6 @@
 define('BASE_PATH', realpath(__DIR__));
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('DATABASE_PATH', realpath(BASE_PATH . "/database"));
 
 chdir(BASE_PATH);
--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,10 +1,12 @@
 <?php
 
-$host = "host.docker.internal";
-$port = "5112";
-$username = "user";
-$password = "password";
-$dbname = "schedule_arranger";
+require_once UTILS_PATH . '/envSetter.util.php';
+
+$host = $databases['pgHost'];
+$port = $databases['pgPort'];
+$username = $databases['pgUser'];
+$password = $databases['pgPassword'];
+$dbname = $databases['pgDB'];
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
