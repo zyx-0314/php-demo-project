71344b77ac2f9af748ca573f1b5dd4561e9a1ff7
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-06-29 18:58:48 +0800

    fix: Correct table name from "user" to "users" in users.model.sql

--- database/users.model.sql
+++ database/users.model.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public."user" (
+CREATE TABLE IF NOT EXISTS public."users" (
     id uuid NOT NULL PRIMARY KEY,
     first_name varchar(225) NOT NULL,
     middle_name varchar(225),
