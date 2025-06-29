e1748e07d1711db86cf668668299c89a91cff75f
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-06-29 21:21:49 +0800

    fix: Correct path definitions for static data and templates in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,10 @@ define('BASE_PATH', realpath(__DIR__));
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
 define('DATABASE_PATH', realpath(BASE_PATH . "/database"));
-define('DUMMIES_PATH', realpath(BASE_PATH . "/staticData/dummies"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
+define('TEMPLATES_PATH', realpath(BASE_PATH . '/components/templates'));
+define('STATICDATAS_PATH', realpath(BASE_PATH . '/staticDatas'));
+define('LAYOUTS_PATH', realpath(BASE_PATH . '/layouts'));
+
 
 chdir(BASE_PATH);
